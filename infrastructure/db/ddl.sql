0d04938e0f0604a6809abab3c0cdbe404ccb625c
Soumyadip <bhowmik.soumyadip9@gmail.com>
2025-05-20 21:51:42 +0530

    refactored the main branch

--- infrastructure/db/ddl.sql
+++ infrastructure/db/ddl.sql
@@ -3,6 +3,7 @@
 CREATE TABLE IF NOT EXISTS users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     full_name TEXT,
+    password TEXT,
     designation TEXT,
     provider TEXT,
     email_address TEXT NOT NULL UNIQUE,
@@ -12,7 +13,7 @@ CREATE TABLE IF NOT EXISTS users (
     phone_number VARCHAR(10) NOT NULL,
     notification_enabled boolean DEFAULT false,
     active boolean DEFAULT true,
-    verified boolean DEFAULT false,
+    verified boolean DEFAULT false
 );
 
 CREATE TABLE IF NOT EXISTS event_logs (
